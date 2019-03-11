eddd6a72fcebe4ed04a9113d54fea253923e1815
shubhamgupta123 <shubhamgupta123@gmail.com>
2019-03-11 08:40:01 +0530

    Updated setup.py for v10 compatibility

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from setuptools import setup, find_packages
 #from pip.req import parse_requirements
 import re, ast
 
-with open(‘requirements.txt’) as f:
-install_requires = f.read().strip().split(’\n’)
-
 # get version from __version__ variable in paperkart/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
 with open('paperkart/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
+
+with open('requirements.txt') as f:
+install_requires = f.read().strip().split('\n')
 
 #requirements = parse_requirements("requirements.txt", session="")
 
