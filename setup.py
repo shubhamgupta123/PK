39fd6815b71d2fc40a435360428465b60ed77fcf
shubhamgupta123 <shubhamgupta123@gmail.com>
2019-03-11 08:44:27 +0530

    changes for compatibility

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-#from pip.req import parse_requirements
+# from pip.req import parse_requirements
 import re, ast
 
 # get version from __version__ variable in paperkart/__init__.py
@@ -24,6 +24,6 @@ setup(
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=[str(ir.req) for ir in requirements],
-	dependency_links=[str(ir._link) for ir in requirements if ir._link]
+	install_requires=install_requires
+	# dependency_links=[str(ir._link) for ir in requirements if ir._link]
 )
