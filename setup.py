59fc702907026b3cdc090ef156db8e39d23c2226
shubhamgupta123 <shubhamgupta123@gmail.com>
2019-03-11 08:48:46 +0530

    few changes

--- setup.py
+++ setup.py
@@ -1,20 +1,16 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-# from pip.req import parse_requirements
 import re, ast
 
 # get version from __version__ variable in paperkart/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
+with open('requirements.txt') as f:
+        install_requires = f.read().strip().split('\n')
+
 with open('paperkart/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
-
-with open('requirements.txt') as f:
-install_requires = f.read().strip().split('\n')
-
-#requirements = parse_requirements("requirements.txt", session="")
-
 setup(
 	name='paperkart',
 	version=version,
@@ -25,5 +21,4 @@ setup(
 	zip_safe=False,
 	include_package_data=True,
 	install_requires=install_requires
-	# dependency_links=[str(ir._link) for ir in requirements if ir._link]
 )
